41878527cc38e62dafd660c7e798a6859da06af9
xander07 <38918899+xander07@users.noreply.github.com>
2022-02-08 10:09:48 -0500

    Correciones 2.0

--- preguntas.py
+++ preguntas.py
@@ -248,10 +248,9 @@ def pregunta_13():
     E    275
     Name: _c5b, dtype: int64
     """
-    aux = pd.merge(
+    m = pd.merge(
         tbl0,
         tbl1,
-        how = 'outer'
+        how="outer",
     )
-
-    return aux.groupby('_c1')['_c5b'].sum()
+    return m.groupby('_c1')['_c5b'].sum()
